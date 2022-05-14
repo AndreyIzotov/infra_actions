e65119f661452fbcd397ff1308499fad2eb83963
Андрей Изотов <andrej.izotoff@yandex.ru>
2022-05-14 14:35:44 +0300

    test telegram bot

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!!! Ура оно работает!!')
 
 
 def second_page(request):
